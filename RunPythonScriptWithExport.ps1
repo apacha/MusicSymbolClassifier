024f6ce773b6edd3d1f5c053ed3ff34f330c2ce3
Alexander Pacha <alexander.pacha@gmail.com>
2017-06-05 22:04:59 +0200

    Added PS-script to run training from command-line and export results

---
+++ RunPythonScriptWithExport.ps1
@@ -0,0 +1,28 @@
+﻿$pathToSourceRoot = "C:\Users\Alex\Repositories\MusicSymbolClassifier\HomusTrainer\"
+$pathToTranscript = $pathToSourceRoot + "output.txt"
+
+# Allowing wider outputs https://stackoverflow.com/questions/7158142/prevent-powergui-from-truncating-the-output
+$pshost = get-host
+$pswindow = $pshost.ui.rawui
+$newsize = $pswindow.buffersize
+$newsize.height = 9999
+$newsize.width = 1500
+$pswindow.buffersize = $newsize
+#$newsize = $pswindow.windowsize
+#$newsize.height = 50
+#$newsize.width = 150
+#$pswindow.windowsize = $newsize
+
+
+
+Start-Transcript -path $pathToTranscript -append
+
+cd $pathToSourceRoot
+echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
+$env:PYTHONPATH = $pathToSourceRoot
+
+#python C:\Users\Alex\Repositories\MusicSymbolClassifier\HomusTrainer\tests\Symbol_test.py
+cd "tests"
+pytest
+
+Stop-Transcript
