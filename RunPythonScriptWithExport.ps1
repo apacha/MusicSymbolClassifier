1815831746727ab2f49a6ffa40ff1b81d8a24f9c
Alexander Pacha <alexander.pacha@gmail.com>
2017-06-06 11:59:16 +0200

    Changed script to run with various hyperparameter-combinations

--- RunPythonScriptWithExport.ps1
+++ RunPythonScriptWithExport.ps1
@@ -1,5 +1,5 @@
 ﻿$pathToSourceRoot = "C:\Users\Alex\Repositories\MusicSymbolClassifier\HomusTrainer\"
-$pathToTranscript = $pathToSourceRoot + "output.txt"
+$pathToTranscript = "$($pathToSourceRoot)output"
 
 # Allowing wider outputs https://stackoverflow.com/questions/7158142/prevent-powergui-from-truncating-the-output
 $pshost = get-host
@@ -13,10 +13,6 @@ $pswindow.buffersize = $newsize
 #$newsize.width = 150
 #$pswindow.windowsize = $newsize
 
-
-
-Start-Transcript -path $pathToTranscript -append
-
 cd $pathToSourceRoot
 echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
 $env:PYTHONPATH = $pathToSourceRoot
@@ -25,6 +21,18 @@ $env:PYTHONPATH = $pathToSourceRoot
 #cd "tests"
 #pytest
 
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --delete_and_recreate_dataset_directory True --model_name vgg -s 3 -offsets 88
+Start-Transcript -path "$($pathToTranscript)_1.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --delete_and_recreate_dataset_directory True --model_name vgg -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
 
+Start-Transcript -path "$($pathToTranscript)_2.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --delete_and_recreate_dataset_directory True --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)_3.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --delete_and_recreate_dataset_directory True --model_name vgg -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 16 --optimizer SGD
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)_4.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --delete_and_recreate_dataset_directory True --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 16 --optimizer SGD
 Stop-Transcript
