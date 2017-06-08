c437669a6d50169c347babd4bc81657ad3a0db55
Alexander Pacha <alexander.pacha@gmail.com>
2017-06-08 11:53:02 +0200

    Fixed width/heigth issue in PS-script

--- RunPythonScriptWithExport.ps1
+++ RunPythonScriptWithExport.ps1
@@ -22,17 +22,17 @@ $env:PYTHONPATH = $pathToSourceRoot
 #pytest
 
 Start-Transcript -path "$($pathToTranscript)_1.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
 Stop-Transcript
 
 Start-Transcript -path "$($pathToTranscript)_2.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --use_existing_dataset_directory --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --use_existing_dataset_directory --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
 Stop-Transcript
 
 #Start-Transcript -path "$($pathToTranscript)_3.txt" -append
-#python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 64 --optimizer SGD
+#python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer SGD
 #Stop-Transcript
 #
 #Start-Transcript -path "$($pathToTranscript)_4.txt" -append
-#python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 192 --height 96 --minibatch_size 64 --optimizer SGD
+#python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer SGD
 #Stop-Transcript
