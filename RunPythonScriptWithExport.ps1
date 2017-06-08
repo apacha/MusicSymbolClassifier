d1e91ae253367549b21d713f28944a588bf7594e
Alexander Pacha <alexander.pacha@gmail.com>
2017-06-08 11:55:40 +0200

    Updated script to yield more meaningful results

--- RunPythonScriptWithExport.ps1
+++ RunPythonScriptWithExport.ps1
@@ -1,5 +1,5 @@
 ﻿$pathToSourceRoot = "C:\Users\Alex\Repositories\MusicSymbolClassifier\HomusTrainer\"
-$pathToTranscript = "$($pathToSourceRoot)output"
+$pathToTranscript = "$($pathToSourceRoot)"
 
 # Allowing wider outputs https://stackoverflow.com/questions/7158142/prevent-powergui-from-truncating-the-output
 $pshost = get-host
@@ -21,18 +21,18 @@ $env:PYTHONPATH = $pathToSourceRoot
 #cd "tests"
 #pytest
 
-Start-Transcript -path "$($pathToTranscript)_1.txt" -append
+Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg_staff74-81-88-95_192x96_no_lr_reduction.txt" -append
 python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
 Stop-Transcript
 
-Start-Transcript -path "$($pathToTranscript)_2.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --use_existing_dataset_directory --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
+Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg4_staff74-81-88-95_192x96_no_lr_reduction.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
 Stop-Transcript
 
-#Start-Transcript -path "$($pathToTranscript)_3.txt" -append
+#Start-Transcript -path "$($pathToTranscript)output_3.txt" -append
 #python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer SGD
 #Stop-Transcript
 #
-#Start-Transcript -path "$($pathToTranscript)_4.txt" -append
+#Start-Transcript -path "$($pathToTranscript)output_4.txt" -append
 #python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg4 -s 3 -offsets 74,81,88,95 --width 96 --height 192 --minibatch_size 64 --optimizer SGD
 #Stop-Transcript
