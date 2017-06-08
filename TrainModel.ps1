aaae4b63457898cb797ea2833b1cba21400231da
Alexander Pacha <alexander.pacha@gmail.com>
2017-06-08 12:49:15 +0200

    Moving started scripts to "Started scripts"-section

--- TrainModel.ps1
+++ TrainModel.ps1
@@ -26,34 +26,6 @@ $env:PYTHONPATH = $pathToSourceRoot
 # Trainings not yet started
 ################################################
 
-##### Evaluating different optimizers on the same data, with/without LR-Reduction
-
-# Not yet started
-Start-Transcript -path "$($pathToTranscript)2017-06-xx_vgg_staff88.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 64 --optimizer SGD
-Stop-Transcript
-
-# Not yet started
-Start-Transcript -path "$($pathToTranscript)2017-06-xx_vgg_staff88.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 64 --optimizer Adadelta
-Stop-Transcript
-
-# Not yet started
-Start-Transcript -path "$($pathToTranscript)2017-06-xx_vgg_staff88.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 64 --optimizer Adadelta --no_dynamic_learning_rate_reduction
-Stop-Transcript
-
-# Not yet started
-Start-Transcript -path "$($pathToTranscript)2017-06-xx_vgg_staff88.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 64 --optimizer Adam
-Stop-Transcript
-
-# Not yet started
-Start-Transcript -path "$($pathToTranscript)2017-06-xx_vgg_staff88.txt" -append
-python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
-Stop-Transcript
-
-
 ##### Evaluating different line-thicknesses
 
 # Not yet started
@@ -107,4 +79,32 @@ Stop-Transcript
 # Started on Donkey, 08.06.2017, 12:00
 Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg4_staff60-67-74-81_192x96_no_lr_reduction.txt" -append
 python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg4 -s 3 -offsets 60,67,74,81 --width 96 --height 192 --minibatch_size 64 --optimizer Adam --no_dynamic_learning_rate_reduction
+Stop-Transcript
+
+
+##### Evaluating different optimizers on the same data, with/without LR-Reduction
+
+# Started on Monsti, 08.06.2017, 12:48
+Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg_staff88_sgd.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 16 --optimizer SGD
+Stop-Transcript
+
+# Started on Monsti, 08.06.2017, 12:48
+Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg_staff88_adadelta.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
+
+# Started on Monsti, 08.06.2017, 12:48
+Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg_staff88_adadelta_no_lr_reduction.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 16 --optimizer Adadelta --no_dynamic_learning_rate_reduction
+Stop-Transcript
+
+# Started on Monsti, 08.06.2017, 12:48
+Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg_staff88_adam.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 16 --optimizer Adam
+Stop-Transcript
+
+# Started on Monsti, 08.06.2017, 12:48
+Start-Transcript -path "$($pathToTranscript)2017-06-08_vgg_staff88_adam_no_lr_reduction.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg -s 3 -offsets 88 --width 128 --height 224 --minibatch_size 16 --optimizer Adam --no_dynamic_learning_rate_reduction
 Stop-Transcript
