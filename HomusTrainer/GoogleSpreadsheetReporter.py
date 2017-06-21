c2f9d365109badbd99a80a0383aab03bfc6d0ded
Alexander Pacha <alexander.pacha@gmail.com>
2017-06-21 13:52:59 +0200

    Completed Google Spreadsheet Reporter and adding client_secret.json to list of ignored files, so it is not committed accidentially

--- HomusTrainer/GoogleSpreadsheetReporter.py
+++ HomusTrainer/GoogleSpreadsheetReporter.py
@@ -12,9 +12,10 @@ from oauth2client.file import Storage
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+MAX_NUMBER_OF_LINES = 400
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+APPLICATION_NAME = 'Google Sheets API Python Training Reporter'
 
 
 def get_credentials():
@@ -42,26 +43,34 @@ def get_credentials():
     return credentials
 
 
-def main():
-    """Shows basic usage of the Sheets API.
-
-    Creates a Sheets API service object and prints the names and majors of
-    students in a sample spreadsheet:
-    https://docs.google.com/spreadsheets/d/1BbrHOoKTIhldz4Eh3tMEM7ne6rUujojyjs00ikO311A/edit#gid=0
+def append_result_to_spreadsheet(dataset_size: int = 15200, image_sizes: str = "192x96px",
+                                 stroke_thicknesses: str = "3", staff_lines: str = "60,67,74,81",
+                                 model_name: str = "vgg4", data_augmentation="20% zoom, 10° rotation",
+                                 optimizer: str = "Adadelta", early_stopping: int = 20, reduction_patience: int = 8,
+                                 learning_rate_reduction_factor: float = 0.5, minibatch_size: int = 64,
+                                 initialization: str = "glorot_uniform", initial_learning_rate: float = 1.0,
+                                 accuracy: float = "0.10", date: str = "24.12.9999"):
+    """ Appends the provided results to the Google Spreadsheets document
+        https://docs.google.com/spreadsheets/d/1D9kHRhrOBogcrr5ko1DleCnHVKGGNkwbBc6_mnfA6XE/edit#gid=0
     """
+    service, spreadsheet_id = get_service_and_spreadsheet_id()
+    first_empty_line = get_first_empty_line_fast(service, spreadsheet_id)
+    print("Uploading results to Google Spreadsheet and appending at first empty line {0}".format(first_empty_line))
+    data = [dataset_size, image_sizes, stroke_thicknesses, staff_lines, model_name, data_augmentation, optimizer,
+            early_stopping, reduction_patience, learning_rate_reduction_factor, minibatch_size, initialization,
+            initial_learning_rate, accuracy, date]
+    write_into_spreadsheet(service, spreadsheet_id, data, first_empty_line)
+
+
+def get_service_and_spreadsheet_id():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
                     'version=v4')
     service = discovery.build('sheets', 'v4', http=http,
                               discoveryServiceUrl=discoveryUrl)
-
-    spreadsheet_id = '1BbrHOoKTIhldz4Eh3tMEM7ne6rUujojyjs00ikO311A'
-
-    first_empty_line = get_first_empty_line(service, spreadsheet_id)
-    print("Inserting at first empty line {0}".format(first_empty_line))
-
-    write_into_spreadsheet(service, spreadsheet_id, ["test", "test2", "05.02.1072"], first_empty_line)
+    spreadsheet_id = '1D9kHRhrOBogcrr5ko1DleCnHVKGGNkwbBc6_mnfA6XE'
+    return service, spreadsheet_id
 
 
 def write_into_spreadsheet(service, spreadsheet_id, row_data: List[str], line_number):
@@ -81,29 +90,12 @@ def write_into_spreadsheet(service, spreadsheet_id, row_data: List[str], line_nu
     return result
 
 
-def get_first_empty_line(service, spreadsheet_id) -> int:
-    for line_number in range(1, 1000):
-        result = get_line_data(service, spreadsheet_id, line_number)
-        if result_contains_data(result):
-            continue
-        return line_number
-
-
-def get_line_data(service, spreadsheet_id, line_number) -> dict:
+def get_first_empty_line_fast(service, spreadsheet_id) -> int:
     result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id, range="Sheet1!A{0}:Z{0}".format(line_number)).execute()
-    return result
-
-
-def result_contains_data(result) -> bool:
-    try:
-        values = result.get('values', [])
-        if not values or not values[0]:
-            return False
-        return True
-    except:
-        return False
+        spreadsheetId=spreadsheet_id, range="Sheet1!A1:A{0}".format(MAX_NUMBER_OF_LINES)).execute()
+    values = result.get('values', [])
+    return len(values) + 1
 
 
 if __name__ == '__main__':
-    main()
+    append_result_to_spreadsheet()
