3a539c1231a65df9d832a31f6e8d5852eb493d4f
Alexander Pacha <alexander.pacha@gmail.com>
2017-06-28 23:36:18 +0200

    Fixed bug in history-plotter that did not allow the new structure

--- HomusTrainer/TrainingHistoryPlotter.py
+++ HomusTrainer/TrainingHistoryPlotter.py
@@ -6,29 +6,33 @@ from matplotlib import pyplot
 class TrainingHistoryPlotter:
     @staticmethod
     def plot_history(history: History, file_name: str, show_plot: bool = True):
-        training_loss = history.history['loss']
-        training_accuracy = history.history['acc']
-        validation_loss = history.history['val_loss']
-        validation_accuracy = history.history['val_acc']
-
         epoch_list = numpy.add(history.epoch, 1)  # Add 1 so it starts with epoch 1 instead of 0
 
         fig = pyplot.figure(1)
         # fig.suptitle('TRAINNING vs VALIDATION', fontsize=14, fontweight='bold')
 
-        fig.add_subplot(211)
-        pyplot.xlabel("Epoch")
-        pyplot.ylabel("Loss")
-        pyplot.plot(epoch_list, training_loss, '--', linewidth=2, label='Training loss')
-        pyplot.plot(epoch_list, validation_loss, label='Validation loss')
-        pyplot.legend(loc='upper right')
-
-        fig.add_subplot(212)
-        pyplot.xlabel("Epoch")
-        pyplot.ylabel("Accuracy")
-        pyplot.plot(epoch_list, training_accuracy, '--', linewidth=2, label='Training accuracy')
-        pyplot.plot(epoch_list, validation_accuracy, label='Validation accuracy')
-        pyplot.legend(loc='lower right')
+        # Regular plot for classification only
+        if "val_acc" in history.history:
+            TrainingHistoryPlotter.add_subplot(epoch_list, fig, history, 211, "Loss", "loss", "Training loss",
+                                               "val_loss",
+                                               "Validation loss", "upper right")
+            TrainingHistoryPlotter.add_subplot(epoch_list, fig, history, 212, "Accuracy", "acc", "Training accuracy",
+                                               "val_acc",
+                                               "Validation accuracy", "lower right")
+        else:
+            TrainingHistoryPlotter.add_subplot(epoch_list, fig, history, 221, "Classification Loss",
+                                               "output_class_loss",
+                                               "Training loss", "val_output_class_loss",
+                                               "Validation loss", "upper right")
+            TrainingHistoryPlotter.add_subplot(epoch_list, fig, history, 222, "Classification Accuracy",
+                                               "output_class_acc", "Training accuracy", "val_output_class_acc",
+                                               "Validation accuracy", "lower right")
+            TrainingHistoryPlotter.add_subplot(epoch_list, fig, history, 223, "Bounding-Box Loss",
+                                               "output_bounding_box_loss", "Training loss",
+                                               "val_output_bounding_box_loss", "Validation loss", "upper right")
+            TrainingHistoryPlotter.add_subplot(epoch_list, fig, history, 224, "Bounding-Box Accuracy",
+                                               "output_bounding_box_acc", "Training accuracy",
+                                               "val_output_bounding_box_acc", "Validation accuracy", "lower right")
 
         # pyplot.subplots_adjust(wspace=0.1)
         pyplot.tight_layout()
@@ -36,3 +40,14 @@ class TrainingHistoryPlotter:
 
         if show_plot:
             pyplot.show()
+
+    @staticmethod
+    def add_subplot(epoch_list, fig, history, subplot_region, y_axis_label,
+                    history_parameter1, parameter1_label,
+                    history_parameter2, parameter2_label, legend_position):
+        fig.add_subplot(subplot_region)
+        pyplot.xlabel("Epoch")
+        pyplot.ylabel(y_axis_label)
+        pyplot.plot(epoch_list, history.history[history_parameter1], '--', linewidth=2, label=parameter1_label)
+        pyplot.plot(epoch_list, history.history[history_parameter2], label=parameter2_label)
+        pyplot.legend(loc=legend_position)
