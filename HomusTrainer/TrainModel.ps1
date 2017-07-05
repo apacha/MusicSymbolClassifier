951b7bec89ccfcba91b6927db23e94a7577ed7c7
Alexander Pacha <alexander.pacha@gmail.com>
2017-07-05 12:01:57 +0200

    Added upcoming trainings

--- HomusTrainer/TrainModel.ps1
+++ HomusTrainer/TrainModel.ps1
@@ -26,6 +26,26 @@ $env:PYTHONPATH = $pathToSourceRoot
 # Upcoming Trainings 
 ################################################
 
+# Started on Monsti, 05.07.2017
+Start-Transcript -path "$($pathToTranscript)2017-07-05_res_net_3_small_staff74_192x96_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_3_small -s 3 -offsets 74 --width 96 --height 192 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)2017-07-05_res_net_3_staff74_192x96_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_3 -s 3 -offsets 74 --width 96 --height 192 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)2017-07-05_res_net_4_staff74_192x96_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_4 -s 3 -offsets 74 --width 96 --height 192 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)2017-07-05_res_net_5_small_staff74_192x96_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_5_small -s 3 -offsets 74 --width 96 --height 192 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)2017-07-05_res_net_5_staff74_192x96_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_5 -s 3 -offsets 74 --width 96 --height 192 --minibatch_size 16 --optimizer Adadelta
+Stop-Transcript
 
 #######################################################
 # Below are configurations that already were 
