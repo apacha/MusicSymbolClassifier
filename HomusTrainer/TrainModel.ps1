9057c37180fd4184a3c10f58a77d287463709c38
Alexander Pacha <alexander.pacha@gmail.com>
2017-07-10 17:47:22 +0200

    Updating TrainModel to contain latest runs

--- HomusTrainer/TrainModel.ps1
+++ HomusTrainer/TrainModel.ps1
@@ -26,6 +26,32 @@ $env:PYTHONPATH = $pathToSourceRoot
 # Upcoming Trainings 
 ################################################
 
+#######################################################
+# Below are configurations that already were 
+# started on a machine and should not run again, 
+# so we will terminate this PS-script here
+# but retain those configurations for documentation
+#######################################################
+exit
+
+# Started on Monsti, 10.07.2017
+Start-Transcript -path "$($pathToTranscript)2017-07-10_vgg4_48x48_no_fixed_canvas_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name vgg4 -s 3 --width 48 --height 48 --minibatch_size 16 --optimizer Adadelta --disable_fixed_canvas_size
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)2017-07-10_res_net_3_small_48x48_no_fixed_canvas_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_3_small -s 3 --width 48 --height 48 --minibatch_size 16 --optimizer Adadelta --disable_fixed_canvas_size
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)2017-07-10_res_net_3_small_48x96_no_fixed_canvas_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_3_small -s 3 --width 48 --height 96 --minibatch_size 16 --optimizer Adadelta --disable_fixed_canvas_size
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)2017-07-10_res_net_3_small_192x96_no_fixed_canvas_Adadelta_mb16.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_3_small -s 3 --width 96 --height 192 --minibatch_size 16 --optimizer Adadelta --disable_fixed_canvas_size
+Stop-Transcript
+
+# Started on Donkey, 10.07.2017
 Start-Transcript -path "$($pathToTranscript)2017-07-10_res_net_3_small_192x96_no_fixed_canvas_Adadelta_mb32.txt" -append
 python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_3_small -s 3 --width 96 --height 192 --minibatch_size 32 --optimizer Adadelta --disable_fixed_canvas_size
 Stop-Transcript
@@ -37,14 +63,6 @@ Stop-Transcript
 Start-Transcript -path "$($pathToTranscript)2017-07-10_res_net_3_small_48x48_no_fixed_canvas_Adadelta_mb32.txt" -append
 python C:/Users/Alex/Repositories/MusicSymbolClassifier/HomusTrainer/TrainModel.py --model_name res_net_3_small -s 3 --width 48 --height 96 --minibatch_size 32 --optimizer Adadelta --disable_fixed_canvas_size
 Stop-Transcript
-
-#######################################################
-# Below are configurations that already were 
-# started on a machine and should not run again, 
-# so we will terminate this PS-script here
-# but retain those configurations for documentation
-#######################################################
-exit
 
 # Started on Donkey, 07.07.2017
 Start-Transcript -path "$($pathToTranscript)2017-07-07_res_net_3_small_192x96_Adam_mb32.txt" -append
