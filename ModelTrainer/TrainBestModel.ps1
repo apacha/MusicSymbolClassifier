296124bd85dd8f909e12575f5e9c7169becee616
Alexander Pacha <alexander.pacha@gmail.com>
2017-07-21 19:24:17 +0200

    Adding script that automatically performs the best training.

---
+++ ModelTrainer/TrainBestModel.ps1
@@ -0,0 +1,22 @@
+﻿$pathToSourceRoot = "C:\Users\Alex\Repositories\MusicSymbolClassifier\ModelTrainer\"
+$pathToTranscript = "$($pathToSourceRoot)"
+
+# Allowing wider outputs https://stackoverflow.com/questions/7158142/prevent-powergui-from-truncating-the-output
+$pshost = get-host
+$pswindow = $pshost.ui.rawui
+$newsize = $pswindow.buffersize
+$newsize.height = 9999
+$newsize.width = 1500
+$pswindow.buffersize = $newsize
+#$newsize = $pswindow.windowsize
+#$newsize.height = 50
+#$newsize.width = 150
+#$pswindow.windowsize = $newsize
+
+cd $pathToSourceRoot
+echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
+$env:PYTHONPATH = $pathToSourceRoot
+
+Start-Transcript -path "$($pathToTranscript)Training_best_model_transcript.txt" -append
+python C:/Users/Alex/Repositories/MusicSymbolClassifier/ModelTrainer/TrainModel.py --disable_fixed_canvas_size
+Stop-Transcript
