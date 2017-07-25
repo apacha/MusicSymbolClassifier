57b4535d55697fb4b4d1528a1065968169f1a814
Alexander Pacha <alexander.pacha@gmail.com>
2017-07-25 20:45:56 +0200

    Added unit-test that verifies that the MUSCIMA++ dataset gets download, extracted and rendered correctly.

---
+++ ModelTrainer/tests/MuscimaPlusPlusImageGeneratorTest.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+import unittest
+from glob import glob
+
+from datasets.AudiverisOmrDatasetDownloader import AudiverisOmrDatasetDownloader
+from datasets.AudiverisOmrImageGenerator import AudiverisOmrImageGenerator
+from datasets.HomusDatasetDownloader import HomusDatasetDownloader
+from datasets.HomusImageGenerator import HomusImageGenerator
+from datasets.MuscimaPlusPlusDatasetDownloader import MuscimaPlusPlusDatasetDownloader
+from datasets.MuscimaPlusPlusImageGenerator import MuscimaPlusPlusImageGenerator
+
+
+class MuscimaPlusPlusImageGeneratorTest(unittest.TestCase):
+
+    def test_download_extract_and_render_bitmaps(self):
+        # Arrange
+        datasetDownloader = MuscimaPlusPlusDatasetDownloader("temp/muscima_pp_raw")
+
+        # Act
+        datasetDownloader.download_and_extract_dataset()
+        image_generator = MuscimaPlusPlusImageGenerator()
+        image_generator.extract_symbols("temp/muscima_pp_raw", "temp/muscima_img")
+        all_image_files = [y for x in os.walk("temp/muscima_img") for y in glob(os.path.join(x[0], '*.png'))]
+        actual_number_of_files = len(all_image_files)
+
+        # Assert
+        self.assertEqual(91134, actual_number_of_files)
+
+        # Cleanup
+        os.remove("MUSCIMA-pp_v0.9.zip")
+        shutil.rmtree("temp")
+
+
+if __name__ == '__main__':
+    unittest.main()
