fa50d81022a9ab5736af14fb3e497ea797b94c89
Alexander Pacha <alexander.pacha@gmail.com>
2017-07-25 14:51:56 +0200

    Completed AudiverisOmrImageGenerator for now (that simply ignores note/stem/flag classes)

--- ModelTrainer/datasets/AudiverisOmrImageGenerator.py
+++ ModelTrainer/datasets/AudiverisOmrImageGenerator.py
@@ -4,13 +4,68 @@ from glob import glob
 from xml.etree import ElementTree
 
 from PIL import Image
-from sympy import Point2D
 
+from datasets.AudiverisOmrSymbol import AudiverisOmrSymbol
 from datasets.ExportPath import ExportPath
-from datasets.Rectangle import Rectangle
 
 
 class AudiverisOmrImageGenerator:
+    def __init__(self) -> None:
+        super().__init__()
+        # This mapping contains the relation between the names of the classes used in the Audiveris OMR-dataset
+        # annotations and the names that were used for the other datasets
+        self.symbol_name_mapping = {"accidentalFlat": "Flat",
+                                    "accidentalNatural": "Natural",
+                                    "accidentalSharp": "Sharp",
+                                    "articStaccatissimoAbove": "Staccatissimo",
+                                    "articStaccatoAbove": "Dot",
+                                    "articTenutoBelow": "Tenuto",
+                                    "augmentationDot": "Dot",
+                                    "barlineDouble": "Barline",
+                                    "barlineSingle": "Barline",
+                                    "brace": "Brace",
+                                    "coda": "Coda",
+                                    "codaSquare": "Coda-Square",
+                                    "dynamicMP": "Other",
+                                    "dynamicPiano": "Other",
+                                    "fClef": "F-Clef",
+                                    "fClefChange": "F-Clef",
+                                    "flag8thDown": "Eighth-Flag",
+                                    "flag8thUp": "Eighth-Flag",
+                                    "flag16thUp": "Sixteenth-Flag",
+                                    "flag32ndUp": "Thirty-Two-Flag",
+                                    "flag128thDown": "Onehundred-Twenty-Eight-Flag",
+                                    "gClef": "G-Clef",
+                                    "gClefChange": "G-Clef",
+                                    "graceNoteAcciaccaturaStemUp": "Eighth-Grace-Note",
+                                    "graceNoteAppoggiaturaStemUp": "Eighth-Note",
+                                    "keyFlat": "Flat",
+                                    "keySharp": "Sharp",
+                                    "noteheadBlack": "Full-Note-Head",
+                                    "noteheadBlackSmall": "Full-Note-Head",
+                                    "noteheadHalf": "Half-Note-Head",
+                                    "noteheadWhole": "Whole-Note",
+                                    "rest8th": "Eighth-Rest",
+                                    "rest16th": "Sixteenth-Rest",
+                                    "rest32nd": "Thirty-Two-Rest",
+                                    "rest64th": "Sixty-Four-Rest",
+                                    "rest128th": "Onehundred-Twenty-Eight-Rest",
+                                    "restHalf": "Whole-Half-Rest",
+                                    "restQuarter": "Quarter-Rest",
+                                    "restWhole": "Whole-Half-Rest",
+                                    "segno": "Segno",
+                                    "stem": "Stem",
+                                    "timeSig4over4": "4-4-Time",
+                                    "timeSigCommon": "Common-Time",
+                                    "timeSigCutCommon": "Cut-Time",
+                                    "tuplet3": "Other"}
+
+        # Ledger lines can not be distinguished from tenuto or whole-half-rest, so we ignore it for now
+        self.ignored_classes = ["ledger"]
+        # For now, we ignore half/full notes, flags and stems too
+        self.ignored_classes.extend(["noteheadBlack", "noteheadBlackSmall", "noteheadHalf", "stem", "flag8thDown",
+                                     "flag8thUp", "flag16thUp", "flag32ndUp", "flag128thDown", ])
+
     def extract_symbols(self, raw_data_directory: str, destination_directory: str):
         """
         Extracts the symbols from the raw XML documents and matching images of the Audiveris OMR dataset into
@@ -33,60 +88,6 @@ class AudiverisOmrImageGenerator:
             self.__extract_symbols(data_pair[0], data_pair[1], destination_directory)
 
     def __extract_symbols(self, xml_file: str, image_file: str, destination_directory: str):
-        class_substitution_map = {"accidentalFlat": "Flat",
-                                  "accidentalNatural": "Natural",
-                                  "accidentalSharp": "Sharp",
-                                  "articStaccatissimoAbove": "Staccatissimo",
-                                  "articStaccatoAbove": "Dot",
-                                  "articTenutoBelow": "Tenuto",
-                                  "augmentationDot": "Dot",
-                                  "barlineDouble": "Barline",
-                                  "barlineSingle": "Barline",
-                                  "brace": "Brace",
-                                  "coda": "Coda",
-                                  "codaSquare": "Coda-Square",
-                                  "dynamicMP": "Other",
-                                  "dynamicPiano": "Other",
-                                  "fClef": "F-Clef",
-                                  "fClefChange": "F-Clef",
-                                  "flag8thDown": "Eighth-Flag",
-                                  "flag8thUp": "Eighth-Flag",
-                                  "flag16thUp": "Sixteenth-Flag",
-                                  "flag32ndUp": "Thirty-Two-Flag",
-                                  "flag128thDown": "Onehundred-Twenty-Eight-Flag",
-                                  "gClef": "G-Clef",
-                                  "gClefChange": "G-Clef",
-                                  "graceNoteAcciaccaturaStemUp": "Eighth-Grace-Note",
-                                  "graceNoteApporggiaturaStempUp": "Eighth-Note",
-                                  "keyFlat": "Flat",
-                                  "keySharp": "Sharp",
-                                  "noteheadBlack": "Full-Note-Head",
-                                  "noteheadBlackSmall": "Full-Note-Head",
-                                  "noteheadHalf": "Half-Note-Head",
-                                  "noteheadWhole": "Whole-Note",
-                                  "rest8th": "Eighth-Rest",
-                                  "rest16th": "Sixteenth-Rest",
-                                  "rest32th": "Thirty-Two-Rest",
-                                  "rest64th": "Sixty-Four-Rest",
-                                  "rest128th": "Onehundred-Twenty-Eight-Rest",
-                                  "restHalf": "Whole-Half-Rest",
-                                  "restQuarter": "Quarter-Rest",
-                                  "restWhole": "Whole-Half-Rest",
-                                  "segno": "Segno",
-                                  "stem": "Stem",
-                                  "timeSig4over4": "4-4-Time",
-                                  "timeSigCommon": "Common-Time",
-                                  "timeSigCutCommon": "Cut-Time",
-                                  "tuplet3": "Other"}
-        note_head_classes_for_refinement = ["Full-Note-Head", "Half-Note-Head"]
-        stem_class_for_refinement = "Stem"
-        flag_classes_for_refinement = ["Eight-Flag", "Sixteenth-Flag", "Thirdy-Two-Flag",
-                                       "Onehundred-Twenty-Eight-Flag"]
-
-        ignored_classes = ["ledger"]
-
-        # Everything about should be moved somewhere else
-
         # xml_file, image_file = 'data/audiveris_omr_raw\\IMSLP06053p1.xml', 'data/audiveris_omr_raw\\IMSLP06053p1.png'
         # xml_file, image_file = 'data/audiveris_omr_raw\\mops-1.xml', 'data/audiveris_omr_raw\\mops-1.png'
         # xml_file, image_file = 'data/audiveris_omr_raw\\mtest1-1.xml', 'data/audiveris_omr_raw\\mtest1-1.png'
@@ -97,60 +98,35 @@ class AudiverisOmrImageGenerator:
 
         file_name_without_extension = os.path.splitext(os.path.basename(xml_file))[0]
         symbols = []
-        stems = []
-        note_heads = []
-        note_head_flags = []
 
         for xml_symbol in xml_symbols:
             symbol_class = xml_symbol.get("shape")
 
-            if symbol_class in ignored_classes:
+            if symbol_class in self.ignored_classes:
                 continue  # Ignored classes will be skipped
 
-            mapped_symbol_class = class_substitution_map[symbol_class]
+            mapped_symbol_class = self.symbol_name_mapping[symbol_class]
 
             bounds = xml_symbol.find("Bounds")
             x, y, width, height = bounds.get("x"), bounds.get("y"), bounds.get("w"), bounds.get("h")
             x, y, width, height = int(float(x)), int(float(y)), int(float(width)), int(float(height))
 
             symbol = AudiverisOmrSymbol(mapped_symbol_class, x, y, width, height)
+            symbols.append(symbol)
 
-            if mapped_symbol_class in note_head_classes_for_refinement:
-                note_heads.append(symbol)
-            elif mapped_symbol_class in flag_classes_for_refinement:
-                note_head_flags.append(symbol)
-            elif mapped_symbol_class == stem_class_for_refinement:
-                stems.append(symbol)
-            else:
-                symbols.append(symbol)
-
-        pairs = []
-
-        for half_note_head in [s for s in note_heads if s.symbol_class == "Half-Note-Head"]:
-            for stem in stems:
-                if Rectangle.overlap(half_note_head, stem):
-                    pairs.append((half_note_head, stem))
-
-        i = 0
-        for pair in pairs:
-            symbol_class = pair[0].symbol_class
-            merge = Rectangle.merge(pair[0], pair[1])
-            bounding_box_with_one_pixel_margin = merge.as_bounding_box_with_margin(1)
+        symbol_number = 0
+        for symbol in symbols:
+            symbol_class = symbol.symbol_class
+            bounding_box_with_one_pixel_margin = symbol.as_bounding_box_with_margin(1)
             symbol_image = image.crop(bounding_box_with_one_pixel_margin)
 
             target_directory = os.path.join(destination_directory, symbol_class)
             os.makedirs(target_directory, exist_ok=True)
 
-            export_path = ExportPath(destination_directory, symbol_class, file_name_without_extension + str(i))
+            export_path = ExportPath(destination_directory, symbol_class,
+                                     file_name_without_extension + str(symbol_number))
             symbol_image.save(export_path.get_full_path())
-            i += 1
-
-
-class AudiverisOmrSymbol(Rectangle):
-    def __init__(self, symbol_class: str, x: int, y: int, width: int, height: int) -> None:
-        super().__init__(Point2D(x, y), width, height)
-        self.symbol_class = symbol_class
-
+            symbol_number += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
