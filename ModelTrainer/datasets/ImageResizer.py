267f985dfddcfac1bcbbb27661634ee234769de6
Alexander Pacha <alexander.pacha@gmail.com>
2017-07-26 18:18:02 +0200

    ImageResizer now reports the progress while resizing

--- ModelTrainer/datasets/ImageResizer.py
+++ ModelTrainer/datasets/ImageResizer.py
@@ -1,19 +1,28 @@
 import os
 from glob import glob
 
+import sys
 from PIL import Image
 
 
 class ImageResizer:
     def resize_all_images(self, image_dataset_directory, width, height, resampling_mode):
         all_images = [y for x in os.walk(image_dataset_directory) for y in glob(os.path.join(x[0], '*.png'))]
+        image_counter = 1
+        number_of_images = len(all_images)
         for image_path in all_images:
+            self.__write_progress("Resizing images {0: >5} of {1}".format(image_counter, number_of_images))
+            image_counter += 1
             img = Image.open(image_path).convert('RGB')
             hw_tuple = (height, width)
             if img.size != hw_tuple:
                 img = img.resize(hw_tuple, resampling_mode)
                 img.save(image_path)
 
+    def __write_progress(self, progress: str):
+        sys.stdout.write('\r')
+        sys.stdout.write(progress)
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     image_resizer = ImageResizer()
