174e8edfea1f034d24ac50951dce732463c18998
Alexander Pacha <alexander.pacha@gmail.com>
2017-10-19 17:53:42 +0200

    Fixed issues in report creation

--- ModelTrainer/reporting/sklearn_reporting.py
+++ ModelTrainer/reporting/sklearn_reporting.py
@@ -94,7 +94,10 @@ def classification_report(y_true, y_pred, labels=None, target_names=None,
                 .format(len(labels), len(target_names))
         )
 
-    last_line_heading = 'avg / total'
+    if average is None:
+        last_line_heading = 'avg / total'
+    else:
+        last_line_heading = average + ' avg'
 
     if target_names is None:
         target_names = [u'%s' % l for l in labels]
@@ -108,7 +111,7 @@ def classification_report(y_true, y_pred, labels=None, target_names=None,
 
     p, r, f1, s = precision_recall_fscore_support(y_true, y_pred,
                                                   labels=labels,
-                                                  average=average,
+                                                  average=None,
                                                   sample_weight=sample_weight)
 
     row_fmt = u'{:>{width}s} ' + u' {:>9.{digits}f}' * 3 + u' {:>9}\n'
@@ -118,12 +121,29 @@ def classification_report(y_true, y_pred, labels=None, target_names=None,
 
     report += u'\n'
 
-    # compute averages
+    if average is None:
+        p, r, f1 = np.average(p, weights=s), np.average(r, weights=s), np.average(f1, weights=s),
+    else:
+        p, r, f1, unused_s = precision_recall_fscore_support(y_true, y_pred,
+                                                      labels=labels,
+                                                      average=average,
+                                                      sample_weight=sample_weight)
+
     report += row_fmt.format(last_line_heading,
-                             np.average(p, weights=s),
-                             np.average(r, weights=s),
-                             np.average(f1, weights=s),
+                             p,
+                             r,
+                             f1,
                              np.sum(s),
                              width=width, digits=digits)
 
     return report
+
+
+if __name__ == '__main__':
+    y_true = [0, 1, 2, 2, 2]
+    y_pred = [0, 0, 2, 2, 1]
+    target_names = ['class 0', 'class 1', 'class 2']
+    print(classification_report(y_true, y_pred, target_names=target_names))
+    print(classification_report(y_true, y_pred, target_names=target_names, average='micro'))
+    print(classification_report(y_true, y_pred, target_names=target_names, average='macro'))
+    print(classification_report(y_true, y_pred, target_names=target_names, average='weighted'))
