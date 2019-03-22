daf21171581283463743fe9c4f61a8bffd3c0dfb
Alexander Pacha <alexander.pacha@gmail.com>
2019-03-22 11:11:51 +0100

    Fixing unit-tests

--- ModelTrainer/tests/OpenOmrImagePreparerTest.py
+++ ModelTrainer/tests/OpenOmrImagePreparerTest.py
@@ -11,11 +11,11 @@ from datasets.OpenOmrImagePreparer import OpenOmrImagePreparer
 class OpenOmrImagePreparerTest(unittest.TestCase):
     def test_download_and_prepare_dataset(self):
         # Arrange
-        datasetDownloader = OpenOmrDatasetDownloader("temp/open_omr_raw2")
+        datasetDownloader = OpenOmrDatasetDownloader()
         expected_number_of_images = 503
 
         # Act
-        datasetDownloader.download_and_extract_dataset()
+        datasetDownloader.download_and_extract_dataset("temp/open_omr_raw2")
         image_generator = OpenOmrImagePreparer()
         image_generator.prepare_dataset("temp/open_omr_raw2", "temp/open_omr_image2")
         all_image_files = [y for x in os.walk("temp/open_omr_image2") for y in glob(os.path.join(x[0], '*.png'))]
