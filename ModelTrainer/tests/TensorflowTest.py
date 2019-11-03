d3934d8519199541ee5ea48bff3f270dab699769
Alexander Pacha <alexander.pacha@gmail.com>
2019-11-03 18:37:36 +0100

    Updating tensorflow test to Tensorflow 2.0

--- ModelTrainer/tests/TensorflowTest.py
+++ ModelTrainer/tests/TensorflowTest.py
@@ -3,9 +3,8 @@ import tensorflow as tf
 
 class TensorflowTest(tf.test.TestCase):
     def test_square(self):
-        with self.test_session():
-            x = tf.square([2, 3])
-            self.assertAllEqual(x.eval(), [4, 9])
+        x = tf.square([2, 3])
+        self.assertAllEqual(x.numpy(), [4, 9])
 
 
 if __name__ == '__main__':
